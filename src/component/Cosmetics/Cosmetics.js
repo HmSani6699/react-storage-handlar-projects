43fec4ccc333753c670d375182d0226c5a057fd2
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-26 12:08:39 +0600

    jaon data load

--- src/component/Cosmetics/Cosmetics.js
+++ src/component/Cosmetics/Cosmetics.js
@@ -2,13 +2,40 @@ import React from 'react';
 import Cosmetic from '../Cosmetic/Cosmetic';
 
 const Cosmetics = () => {
+    // const cosmetics = [
+    //     { id: 1, name: "Alta", price: 100 },
+    //     { id: 2, name: "malta", price: 200 },
+    //     { id: 3, name: "palta", price: 300 },
+    //     { id: 4, name: "nalta", price: 400 },
+    //     { id: 5, name: "salta", price: 500 }
+    // ]
     const cosmetics = [
-        { id: 1, name: "Alta", price: 100 },
-        { id: 2, name: "malta", price: 200 },
-        { id: 3, name: "palta", price: 300 },
-        { id: 4, name: "nalta", price: 400 },
-        { id: 5, name: "salta", price: 500 }
-    ]
+        {
+            "id": "641fe093c3e6522c13660699",
+            "price": 22,
+            "name": "Gibson Mcneil"
+        },
+        {
+            "id": "641fe0935c44a77ae49ed228",
+            "price": 27,
+            "name": "Susanne Morris"
+        },
+        {
+            "id": "641fe09392ece2abf2ba5b5b",
+            "price": 25,
+            "name": "Sloan Cantu"
+        },
+        {
+            "id": "641fe093d8047120a2a76a9f",
+            "price": 29,
+            "name": "Wong Wilcox"
+        },
+        {
+            "id": "641fe093c6c7580e75ba5f92",
+            "price": 27,
+            "name": "Walsh Parsons"
+        }
+    ];
     return (
         <div>
             <h1>Hallo cosmetics laver</h1>
