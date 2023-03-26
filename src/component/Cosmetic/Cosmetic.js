b2a4142fb1815ea5ea4de4797c818ea40298b85b
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-26 12:43:56 +0600

    ad to event handlar in button width parameter

--- src/component/Cosmetic/Cosmetic.js
+++ src/component/Cosmetic/Cosmetic.js
@@ -1,18 +1,35 @@
 import React from 'react';
+import './Cosmetic.css'
 const cosmetic = {
     border: '2px solid gray',
     margin: "20px",
     borderRadius: '20px',
-    backgroundColor: 'cadetblue'
+    backgroundColor: 'cadetblue',
+    padding: '20px'
 }
+const addBtnStyle = {
+    padding: '8px 15px',
+    borderRadius: '15px',
+    border: '2px solid gray',
+    backgroundColor: 'orange',
+    color: 'white'
+}
+
+
+const addButton = (id) => console.log(id);
+
+
+
 
 const Cosmetic = (props) => {
     const { name, price, id } = props.cosmetic;
+    // const addToCart = () => addButton(id);
     return (
         <div style={cosmetic}>
             <h2>Buy this: {name}</h2>
             <p>Only for: ${price}</p>
             <p><small>it has id: {id}</small></p>
+            <button onClick={() => addButton(id)} style={addBtnStyle}>Add to cart</button>
         </div>
     );
 };
